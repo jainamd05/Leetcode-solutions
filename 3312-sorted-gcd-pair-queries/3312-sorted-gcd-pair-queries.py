fcf9bd5fbbf4d67a1bcd11bd13f129bfde14407b
Jainam Dhiraaj Dudhediya <jainam.dudhediya123@gmail.com>
2026-07-17 09:44:12 +0530

    Time: 559 ms (42.5%), Space: 40.1 MB (62.5%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.py
@@ -0,0 +1,37 @@
+import math
+class Solution:
+    def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
+        # n = len(nums)
+        # gcdPairs = []
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         gcdPairs.append(math.gcd(nums[i], nums[j]))
+
+        # gcdPairs.sort()
+
+        # ans = []
+        # for i in range(len(queries)):
+        #     val = gcdPairs[queries[i]]
+        #     ans.append(val)
+
+        # return ans
+        m = max(nums)
+        cnt = [0] * (m + 1)
+        for num in nums:
+            cnt[num] += 1
+        for i in range(1, m + 1):
+            for j in range(i * 2, m + 1, i):
+                cnt[i] += cnt[j]
+        for i in range(1, m + 1):
+            cnt[i] = cnt[i] * (cnt[i] - 1) // 2
+        for i in range(m, 0, -1):
+            for j in range(i * 2, m + 1, i):
+                cnt[i] -= cnt[j]
+        for i in range(1, m + 1):
+            cnt[i] += cnt[i - 1]
+        ans = []
+        for q in queries:
+            q += 1
+            pos = bisect_left(cnt, q)
+            ans.append(pos)
+        return ans
